7bb83456dd89ee1cd117e0d8fd4b6a460222584b
Hope Hutman <hhutman@telefonprojekt.com>
2019-04-06 12:37:37 -0400

    questions

---
+++ page2.js
@@ -0,0 +1,15 @@
+
+
+const questions = [ {question: 'Nobody respects women more than me', ans1:'President Donald J. Trump', ans2: 'R.Kelly', ans3: 'Henry The 8th'},
+{question: 'Why can\'t we use nuclear weapons?', ans1: 'Alexander Putin', ans2: 'Supreme Leader Ayatollah Ali Khamenei  ', ans3: 'President Donald J. Trump'},
+{question: 'We won with the poorly educated.  I love the poorly educated', ans1: 'Betsy DeVos', ans2: 'President Donald J. Trump', ans3: 'Governor George Wallace'},
+{question: 'I know more about ISIS than the generals do, believe me', ans1: 'President Donald J. Trump', ans2: 'Kim Jong-un', ans3: 'Shimon Peres'},
+{question: 'The concept of global warming was created by and for the Chinese in order to make U.S. manufacturing non-competitive', ans1: 'Rush Limbaugh', ans2: 'Donald J. Trump', ans3: 'Sarah Palin'} ]
+
+
+
+
+//export const getRandomquestions = () => {
+  //const randomIndex = Math.floor(Math.random() * question.length)
+  //return description[randomIndex]
+//}
